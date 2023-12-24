macuyler/files

commit ca980550af95fde09e803d4035051f435db85011

Add file path persistence

src/main.js
@@ -6,18 +6,32 @@ const pathDiv = document.getElementById('path');
 let path = '';
 
 function setPath(p) {
+	let pathParam = `/?path=${encodeURIComponent(p)}`;
+	if (p === '' || p === '/') pathParam = '/';
+	window.history.pushState({}, '', pathParam);
+	updatePath(p);
+}
+
+function updatePath(p) {
 	path = p;
 	render();
 }
 
 function getPathData() {
 	let data = map;
+	const validPath = [];
 	path.split('/')
-		.forEach(function(key) {
+		.every(function(key) {
+			if (key === '') return true;
 			if (Object.keys(data).includes(key)) {
 				data = data[key];
+				validPath.push(key);
+				return true;
 			}
+			return false;
 		});
+	const vp = '/'.concat(validPath.join('/'));
+	if (path !== vp) setPath(vp);
 	return data;
 }
 
@@ -80,5 +94,11 @@ function render() {
 	mapDiv.innerHTML = renderData(getPathData());
 }
 
-render();
+function loadPath() {
+	const params = new URLSearchParams(window.location.search);
+	const p = params.get('path');
+	if (p) updatePath(p);
+}
 
+loadPath();
+render();

commit 5e7533c40c9e5371f0a6c5dc0814072cf05e232d

Fix root files links have an invalid href
When rendering files in the root directory, an inadvertent `/` was being
prepended to the file path that is rendered as the href value. This
issue was introduced in ca980550af95fde09e803d4035051f435db85011. We
would always prepend a `/` character to the persisted path. In the case
of an empty persisted path, the current path would still be set to `/`
instead of a blank string as expected.

src/main.js
@@ -18,6 +18,7 @@ function updatePath(p) {
 }
 
 function getPathData() {
+	let vp = '';
 	let data = map;
 	const validPath = [];
 	path.split('/')
@@ -30,7 +31,7 @@ function getPathData() {
 			}
 			return false;
 		});
-	const vp = '/'.concat(validPath.join('/'));
+	if (validPath.length) vp = '/'.concat(validPath.join('/'));
 	if (path !== vp) setPath(vp);
 	return data;
 }